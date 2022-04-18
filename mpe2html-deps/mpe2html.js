7f78df953479ef79062125118581dffc0244a90e
murongxixi <murongxixi@gmail.com>
2022-04-18 11:16:25 +0800

    update

--- mpe2html-deps/mpe2html.js
+++ mpe2html-deps/mpe2html.js
@@ -1,12 +1,16 @@
 //node.js
+const path = require("path");
 const mume = require("@shd101wyy/mume");
 
 const config = {
+  // Default config directory; `null` means "~/.config/mume"
+  configPath: null,
+
   //Enable this option will render markdown by pandoc instead of markdown-it.
   usePandocParser: false,
 
-  //In Markdown, a single newline character doesn't cause a line break in the generated HTML. In GitHub Flavored Markdown, that is not true. Enable this config option to insert line breaks in rendered HTML for single newlines in Markdown source.
-  breakOnSingleNewLine: false,
+  // In Markdown, a single newline character doesn't cause a line break in the generated HTML. In GitHub Flavored Markdown, that is not true. Enable this config option to insert line breaks in rendered HTML for single newlines in Markdown source.
+  breakOnSingleNewLine: true, // I modify it to false
 
   //Enable smartypants and other sweet transforms.
   enableTypographer: false,
@@ -24,10 +28,14 @@ const config = {
     ["$$", "$$"],
     ["\\[", "\\]"],
   ],
+
   mathRenderingOnLineService: "https://latex.codecogs.com/gif.latex", //"https://latex.codecogs.com/svg.latex", "https://latex.codecogs.com/png.latex"
 
-  //Enable Wiki Link syntax support. More information can be found a  https://help.github.com/articles/adding-links-to-wikis/
+  // Enable Wiki Link syntax support. More information can be found a  https://help.github.com/articles/adding-links-to-wikis/
   enableWikiLinkSyntax: true,
+
+  // If checked, we use GitHub style piped wiki links, i.e. [[linkText|wikiLink]]. Otherwise, we use [[wikiLink|linkText]] as the original Wikipedia style.
+  useGitHubStylePipedLink: true,
 
   //By default, the extension for wikilink is `.md`. For example: [[test]] will direct to file path `test.md`.
   wikiLinkFileExtension: ".md",
@@ -36,12 +44,12 @@ const config = {
   enableEmojiSyntax: true,
 
   //Enable extended table syntax to support merging table cells.
-  enableExtendedTableSyntax: true,
+  enableExtendedTableSyntax: true, // I modify it to true
 
   //Enable CriticMarkup syntax. Only works with markdown-it parser.
 
   //Please check http://criticmarkup.com/users-guide.php for more information.
-  enableCriticMarkupSyntax: true,
+  enableCriticMarkupSyntax: true, // I modify it to true
 
   //Front matter rendering option
   frontMatterRenderingOption: "none", //'none' | 'table' | 'code block'
@@ -52,7 +60,7 @@ const config = {
   //Code Block theme
 
   //If `auto.css` is chosen, then the code block theme that best matches the current preview theme will be picked.
-  codeBlockTheme: "github.css", // "auto.css", "default.css", "atom-dark.css", "atom-light.css", "atom-material.css", "coy.css", "darcula.css", "dark.css", "funky.css", "github.css", "hopscotch.css", "monokai.css", "okaidia.css", "one-dark.css", "one-light.css", "pen-paper-coffee.css", "pojoaque.css", "solarized-dark.css", "solarized-light.css", "twilight.css", "vue.css", "vs.css", "xonokai.css"
+  codeBlockTheme: "solarized-light.css", // "auto.css", "default.css", "atom-dark.css", "atom-light.css", "atom-material.css", "coy.css", "darcula.css", "dark.css", "funky.css", "github.css", "hopscotch.css", "monokai.css", "okaidia.css", "one-dark.css", "one-light.css", "pen-paper-coffee.css", "pojoaque.css", "solarized-dark.css", "solarized-light.css", "twilight.css", "vue.css", "vs.css", "xonokai.css"
 
   //Preview theme
   previewTheme: "none.css",
@@ -67,7 +75,7 @@ const config = {
   imageFolderPath: "/assets",
 
   //Whether to print background for file export or not. If set to `false`, then `github-light` preview theme will b  used. You can also set `print_background` in front-matter for individual files.
-  printBackground: true,
+  printBackground: true, // I modify it to true
 
   //Chrome executable path, which is used for Puppeteer export. Leaving it empty means the path will be found automatically.
   chromePath: "",
@@ -92,7 +100,7 @@ const config = {
   //⚠ ️ Please Use This Feature With Caution Because It May Put Your Security At Risk!
 
   //   Your machine can get hacked if someone makes you open a markdown with malicious code while script execution is enabled.
-  enableScriptExecution: true,
+  enableScriptExecution: true, // I modify it to true
 
   //Enables transform audio video link to to html5 embed audio video tags.
 
@@ -119,8 +127,13 @@ const config = {
   puppeteerWaitForTimeout: 0,
 
   //If set to true, then locally installed puppeteer-core will be required. Otherwise, the puppeteer globally installed by `npm install -g puppeteer` will be required.
-
   usePuppeteerCore: true,
+
+  // Args passed to puppeteer.launch({args: $puppeteerArgs})
+  puppeteerArgs: [],
+
+  // Render using PlantUML server instead of binary. Leave it empty to use the builtin plantuml.jar binary (`java` is required in system path). Eg: "http://localhost:8080/svg/".
+  plantumlServer: "",
 };
 
 async function main() {
@@ -131,12 +144,7 @@ async function main() {
     config: config,
   });
 
-  //open in browser
-
-  //await engine.openInBrowser({ runAllCodeChunks: true });
-
   //html export
-
   await engine.htmlExport({ offline: true, runAllCodeChunks: true });
 
   return process.exit();
